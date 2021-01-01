00d71aabdfda54b6ff57277e6d4caa610a980cf1
Labnan <khalidmasum@iut-dhaka.edu>
2021-01-01 22:48:53 +0600

    Guidelines for adding customer to database or finding a customer from database

--- src/bookcorner/models/Customer.java
+++ src/bookcorner/models/Customer.java
@@ -5,10 +5,13 @@ public class Customer extends Model {
     String name;
     String address;
 
+    boolean recordable = false;
+
     public Customer(String contactNumber, String name, String address) {
         this.contactNumber = contactNumber;
         this.name = name;
         this.address = address;
+        recordable = true;
     }
 
     public Customer(String contactNumber){
@@ -22,6 +25,15 @@ public class Customer extends Model {
                         book.getQuantity()+
                         ")"
                 );
+    }
+
+
+    public void addToRecords(){
+        if(!recordable) return;
+
+        /*Todo: Add this customer to the database
+                Using procedure: add_or_update_customer()
+        */
     }
 
 }
