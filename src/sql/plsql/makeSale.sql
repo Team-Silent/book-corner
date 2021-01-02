f15af6b08ee029453912e7cad6e661a18bc69bba
Shahriar Asfaq <55997420+ShahriarAsfaq@users.noreply.github.com>
2021-01-03 01:04:12 +0600

    procedure to add sales info with auto sequence id

---
+++ src/sql/plsql/makeSale.sql
@@ -0,0 +1,30 @@
+CREATE SEQUENCE sales_seq START WITH 1;
+
+
+CREATE OR REPLACE TRIGGER sales_bir 
+BEFORE INSERT ON Sales 
+FOR EACH ROW
+
+BEGIN
+  SELECT sales_seq.NEXTVAL
+  INTO   :new.Sales_id
+  FROM   dual;
+END;
+/
+
+
+
+CREATE OR REPLACE PROCEDURE makeSales(
+                        v_customerid Sales.Customer_id%TYPE
+                        )
+    IS
+
+    
+    
+
+BEGIN
+   
+       INSERT INTO Sales(Customer_id,Sales_Date) VALUES(v_customerid,sysdate);
+ 
+END;
+/
