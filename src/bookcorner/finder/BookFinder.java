b2b44b31c23b36a6bcbb5ab2f0cfeccd815b1bb4
Labnan <khalidmasum@iut-dhaka.edu>
2021-01-02 20:46:23 +0600

    Implemented book finder (untested)

--- src/bookcorner/finder/BookFinder.java
+++ src/bookcorner/finder/BookFinder.java
@@ -2,16 +2,34 @@ package bookcorner.finder;
 
 import bookcorner.models.Book;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class BookFinder extends Finder <Book> {
     @Override
     ArrayList<Book> findAll() {
-        /*
-        Todo: Query to find books and send them as array list
+        ArrayList<Book> bookArrayList = new ArrayList<>();
+        ResultSet resultSet = databaseConnection.runQuery("Select * from Books");
+        try {
+            while (resultSet.next()){
+                Book book = getBookFromResult(resultSet);
+                bookArrayList.add(book);
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return bookArrayList;
+    }
 
-
-         */
-        return null;
+    private Book getBookFromResult(ResultSet resultSet) throws SQLException {
+        return new Book(
+                            resultSet.getString("title"),
+                            resultSet.getString("author"),
+                            resultSet.getInt("purchase_price"),
+                            resultSet.getInt("selling_price"),
+                            resultSet.getInt("stock")
+                    );
     }
 }
