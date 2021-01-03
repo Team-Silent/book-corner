4ad040e326bbd9459dcd4d307df5ed83afc9d33f
Labnan <khalidmasum@iut-dhaka.edu>
2021-01-03 22:20:01 +0600

    refactored findBook

--- src/bookcorner/finder/BookFinder.java
+++ src/bookcorner/finder/BookFinder.java
@@ -11,9 +11,15 @@ public class BookFinder extends Finder <Book> {
     @Override
     List<Book> findAll() {
         List<Book> bookArrayList = new ArrayList<>();
+        String query = "Select * from Books";
+        return getBooks(query);
+    }
 
+
+    private List<Book> getBooks( String query) {
+        List<Book> bookArrayList = new ArrayList<>();
         databaseConnection.connect();
-        ResultSet resultSet = databaseConnection.runQuery("Select * from Books");
+        ResultSet resultSet = databaseConnection.runQuery(query);
         try {
 
             while (resultSet.next()){
@@ -42,21 +48,15 @@ public class BookFinder extends Finder <Book> {
 
     public Book findByID(String id){
         Book book = null;
-        databaseConnection.connect();
-        ResultSet resultSet = databaseConnection.runQuery("Select * from Books where Books.Book_ID = '" + id +"'");
-        try {
-
-            System.out.println("start");
-            while (resultSet.next()){
-                book = getBookFromResult(resultSet);
-
-            }
-        }
-        catch (SQLException e){
-            System.out.println("Exception");
-            e.printStackTrace();
-        }
-        databaseConnection.disconnect();
-        return book;
+        String query = "Select * from Books where Books.Book_ID = '" + id +"'";
+        return getBooks(query).get(0);
     }
+
+    public List<Book> findByProperty(String columnName, String value){
+        String query = "Select * from Books where"+ "Books."+ columnName +"= '" + value +"'";
+        return getBooks(query);
+    }
+
+
+
 }
