04f81b1bf46a3de7276bfb34a42c7ead9e504e7c
Labnan <khalidmasum@iut-dhaka.edu>
2021-01-04 00:16:59 +0600

    dummy data for testing

---
+++ src/sql/seed/seed.sql
@@ -0,0 +1,18 @@
+insert into CUSTOMERS VALUES ('1','customer_1','address_1');
+insert into CUSTOMERS VALUES ('2','customer_2','address_2');
+insert into CUSTOMERS VALUES ('3','customer_3','address_3');
+insert into CUSTOMERS VALUES ('4','customer_4','address_4');
+insert into CUSTOMERS VALUES ('5','customer_5','address_5');
+
+insert into BOOKS VALUES ('book1author1','book1','author1',1000,1900,10);
+insert into BOOKS VALUES ('book2author2','book2','author2',2000,2900,12);
+insert into BOOKS VALUES ('book3author3','book3','author3',3000,3900,15);
+insert into BOOKS VALUES ('book4author4','book4','author4',4000,4900,8);
+insert into BOOKS VALUES ('book5author5','book5','author5',5000,5900,11);
+
+insert into SALES values (1,1,sysdate);
+insert into SALES values (2,2,sysdate);
+insert into SALES values (3,4,sysdate);
+insert into SALES values (4,1,sysdate);
+insert into SALES values (5,3,sysdate);
+insert into SALES values (6,5,sysdate);
