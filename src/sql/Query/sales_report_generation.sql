fe724fc6e5faa843b464f90fa440ef0a3fd0d560
Shahriar Asfaq <shahriarasfaq@iut-dhaka.edu>
2021-02-04 22:46:39 +0600

    update of sales report

--- src/sql/Query/sales_report_generation.sql
+++ src/sql/Query/sales_report_generation.sql
@@ -1,7 +1,6 @@
 SELECT Sales_id, Customers.Name, TO_CHAR( Sales_Date, 'HH24:MI:SS' )
 From Sales, Customers
-Where TO_CHAR( Sales_Date, 'DD-Mon-YYYY' )=TO_CHAR( sysdate, 'DD-Mon-YYYY' ) AND Sales.Customer_id=Customers.Contact_Number;
-
+Where TO_CHAR(Sales_Date,'DD-MM-YYYY')='04-02-2021' AND Sales.Customer_id=Customers.Contact_Number;
 
 SELECT Book_id, Quantity
 FROM Book_Sales_junction
